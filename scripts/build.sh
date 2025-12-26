f4416449513b8e0723dbe6221eb0e1839e522fb5
Sacha <sacha.colbert-lisbona@kalisio.com>
2025-12-26 10:07:30 +0100

    feat: implement Sonar with new CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,8 +15,12 @@ WORKSPACE_DIR="$(dirname "$ROOT_DIR")"
 PUBLISH=false
 JOB_VARIANT=
 WORKFLOW_JOB_ID=
-while getopts "pr:v:" option; do
+RUN_SONAR=false
+while getopts "spr:v:" option; do
     case $option in
+        s) # enable SonarQube analysis and publish code quality & coverage results
+            RUN_SONAR=true
+            ;;
         p) # publish
             PUBLISH=true
             ;;
@@ -38,6 +42,7 @@ done
 
 load_env_files "$WORKSPACE_DIR/development/common/kalisio_dockerhub.enc.env"
 load_value_files "$WORKSPACE_DIR/development/common/KALISIO_DOCKERHUB_PASSWORD.enc.value"
+. "$WORKSPACE_DIR/development/workspaces/jobs/jobs.sh" k-atlas
 
 ## Build job
 ##
